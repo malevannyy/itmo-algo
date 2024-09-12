e7c620fe3ed6a6a69ae254c742d6edc0ba47ee65
engine <engine.tag@gmail.com>
2024-09-13 02:11:23 +0300

    (1)(a) naive solution

--- main.cpp
+++ main.cpp
@@ -1,28 +1,70 @@
 #include <iostream>
 
+#define E int
+
 using namespace std;
 
 class Deque;
 
 class SimpleDeque;
 
+class Node {
+public:
+    Node(E e, Node *prev, Node *next) {
+        this->e = e;
+        this->prev = prev;
+        this->next = next;
+    }
+
+    ~Node() = default;
+
+    // todo private
+    E e;
+    Node *next;
+    Node *prev;
+};
+
+
 class Deque {
 public:
     virtual ~Deque() = default;
 
-    virtual void push(int e) = 0;
+    virtual void push(E e) = 0;
 
-    virtual int pop() = 0;
+    virtual E pop() = 0;
 };
 
 class SimpleDeque : public Deque {
+private:
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
 public:
-    void push(int e) override {
+    SimpleDeque() = default;
 
-    }
+    void push(E e) override {
+        const auto node = new Node(e, nullptr, head);
+        if (head != nullptr) {
+            head = node;
+        }
+        if (tail == nullptr) {
+            tail = node;
+        }
+        head = node;
+    };
 
-    int pop() override {
-        return 0;
+    E pop() override {
+        if (head == nullptr) {
+            throw new exception;
+        }
+        const auto e = head->e;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+        return e;
     }
 
 };
@@ -42,7 +84,7 @@ int main() {
                 break;
             case '-' :
                 const auto x = stack->pop();
-                cout << x;
+                cout << x << endl;
                 break;
         }
     }
