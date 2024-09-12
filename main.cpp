838dd6ec96dcf042a7c671daca7e11d847bd58e7
engine <engine.tag@gmail.com>
2024-09-13 01:40:54 +0300

    (1)(a) add wireframe

--- main.cpp
+++ main.cpp
@@ -2,9 +2,51 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+class Deque;
 
+class SimpleDeque;
+
+class Deque {
+public:
+    virtual ~Deque() = default;
+
+    virtual void push(int e) = 0;
+
+    virtual int pop() = 0;
+};
+
+class SimpleDeque : public Deque {
+public:
+    void push(int e) override {
+
+    }
+
+    int pop() override {
+        return 0;
+    }
+
+};
+
+int main() {
+    int n;
+    cin >> n;
+    const auto stack = new SimpleDeque();
+    for (int i = 0; i < n; i++) {
+        char op;
+        cin >> op;
+        switch (op) {
+            case '+' :
+                int message;
+                cin >> message;
+                stack->push(message);
+                break;
+            case '-' :
+                const auto x = stack->pop();
+                cout << x;
+                break;
+        }
+    }
+
+    delete stack;
     return 0;
 }
-
