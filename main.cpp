01762d5a31ac05c4f184733900f0953edea68392
engine <engine.tag@gmail.com>
2024-09-13 06:52:14 +0300

    (1)(a) stack solution

--- main.cpp
+++ main.cpp
@@ -4,66 +4,37 @@
 
 using namespace std;
 
-class Deque;
-
-class SimpleDeque;
-
-class Node {
-public:
-    Node(E e, Node *prev, Node *next) {
+struct Node {
+    Node(E e, Node *next) {
         this->e = e;
-        this->prev = prev;
         this->next = next;
     }
 
     ~Node() = default;
 
-    // todo private
     E e;
     Node *next;
-    Node *prev;
 };
 
-
-class Deque {
-public:
-    virtual ~Deque() = default;
-
-    virtual void push(E e) = 0;
-
-    virtual E pop() = 0;
-};
-
-class SimpleDeque : public Deque {
+class Stack {
 private:
     Node *head = nullptr;
-    Node *tail = nullptr;
 
 public:
-    SimpleDeque() = default;
+    Stack() = default;
 
-    void push(E e) override {
-        const auto node = new Node(e, nullptr, head);
-        if (head != nullptr) {
-            head = node;
-        }
-        if (tail == nullptr) {
-            tail = node;
-        }
-        head = node;
+    void push(E e) {
+        head = new Node(e, head);
     };
 
-    E pop() override {
+    E pop() {
         if (head == nullptr) {
             throw new exception;
         }
         const auto e = head->e;
+        const auto ex = head;
         head = head->next;
-        if (head != nullptr) {
-            head->prev = nullptr;
-        } else {
-            tail = nullptr;
-        }
+        delete ex;
         return e;
     }
 
@@ -72,7 +43,7 @@ public:
 int main() {
     int n;
     cin >> n;
-    const auto stack = new SimpleDeque();
+    const auto stack = new Stack();
     for (int i = 0; i < n; i++) {
         char op;
         cin >> op;
@@ -83,8 +54,7 @@ int main() {
                 stack->push(message);
                 break;
             case '-' :
-                const auto x = stack->pop();
-                cout << x << endl;
+                cout << stack->pop() << endl;
                 break;
         }
     }
