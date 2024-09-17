f70d1a666f92c250538598d09c804df3cdbddb67
engine <engine.tag@gmail.com>
2024-09-18 00:03:16 +0300

    (1)(a) `cout` & `endl` is evil, `printf` is 5x faster. Task A Done.

---
+++ test.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <chrono>
+#include "main.cpp"
+
+#define nl '\n'
+
+using namespace std::chrono;
+using namespace std;
+
+duration<long long int, ratio<1, 1000>> now() {
+    return duration_cast<milliseconds>(
+            system_clock::now().time_since_epoch()
+    );
+}
+
+void foo() {
+
+    auto stack = Stack();
+
+    for (int i = 0; i < 1000000; ++i) {
+        stack.push(i);
+    }
+
+    for (int i = 0; i < 1000000; ++i) {
+        int e = stack.pop();
+        printf("%d\n", e);
+        // cout << e << nl;
+    }
+
+}
+
+int main1(int argc, char **argv) {
+    std::ios::sync_with_stdio(false);
+
+    auto start = high_resolution_clock::now();
+
+    foo();
+
+    auto finish = high_resolution_clock::now();
+    duration<double, std::milli> elapsed = finish - start;
+
+    cout << "Elapsed Time: " << elapsed.count() << std::endl;
+
+    return 0;
+}
+
