d2cac9c158166763cc39fbf09f7880c2dcf47650
engine <engine.tag@gmail.com>
2024-11-23 12:52:33 +0300

    3C (new way local ok)

--- 3/C/main.cpp
+++ 3/C/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #pragma ide diagnostic ignored "cert-err34-foo"
 
 const auto m = 8u; // 100000
-const auto magic_number = 3u;
+const auto magic_number = 3u; // 17 depends on m
 const auto magic_mask = (1u << magic_number) - 1;
 const auto number = 1u << magic_number;
 
@@ -21,9 +21,11 @@ int main() {
     unsigned old_imagine;
     scanf("%d", &n);
     for (; shift < n; shift <<= 1);
-    cout << "  3   5   1   4   6   6   3   5   1   3   5   5   6   5   1   4   6   6\n";
+    cout
+            << "  3   5   1   4   6   6   3   5   1   3   5   5   6   5   1   4   6   6\n";
     // cout << " 20  21  16  18  19  18  18  15  10  12  13  12   8   9   4   6   7   6\n";
-    cout << " 20  21  16  18  19  18  14  15  10  11  12  11  11   9   4   6   7   6\n";
+    cout
+            << " 20  21  16  18  19  18  14  15  10  11  12  11  11   9   4   6   7   6\n";
 
     for (unsigned i = n; i > 0; i--) {
         unsigned hi;
@@ -35,23 +37,23 @@ int main() {
         auto imagine = x / m;
         auto real = x % m;
 
-        cout << imagine << '|' << real << " ";cout.flush();
+        cout << imagine << '|' << real << " ";
+        cout.flush();
 
         old_imagine = a[real] >> magic_number;
 
         if (imagine == old_imagine) {
             a[real]++;
         } else {
-            count = +foo(a[real] & magic_mask);
+            auto accum = a[real] & magic_mask;
+            count += foo(accum);
             a[real] = (imagine << magic_number) + 1;
         }
     }
 
-    for (unsigned ic: a) {
-        auto imagine = ic >> magic_number;
-        if (imagine == old_imagine) {
-            count += foo(ic & magic_mask);
-        }
+    for (unsigned tuple: a) {
+        auto accum = tuple & magic_mask;
+        count += foo(accum);
     }
 
     printf("\n");
