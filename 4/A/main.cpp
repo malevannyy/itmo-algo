014b050f63e9770d5a409ea1e79d6c95c0ad995c
engine <engine.tag@gmail.com>
2024-12-13 20:58:54 +0300

    4A (full)

--- 4/A/main.cpp
+++ 4/A/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <vector>
-#define T vector<int>
+
 using namespace std;
 
-T prefix_fun(string s) {
-    int n = (int) s.size();
-    vector<int> p(n, 0);
+void prefix_fun(int *p, const char *s, int n) {
+    for (int i = 0; i < n; i++) {
+        p[i] = 0;
+    }
     for (int i = 1; i < n; i++) {
         int cur = p[i - 1];
         while (s[i] != s[cur] && cur > 0)
@@ -13,35 +13,38 @@ T prefix_fun(string s) {
         if (s[i] == s[cur])
             p[i] = cur + 1;
     }
-    return p;
 }
 
-void foo(T a) {
-    for(int i = 0; i<a.size(); i++) {
+void foo(int *a, int n) {
+    for (int i = 0; i < n; i++) {
         cout << a[i];
     }
     cout << endl;
 }
 
 int main() {
-
-    int bullshit[200000];
-    auto bullshit_index = 0;
-
     string t;
     string s;
     getline(cin, t);
     getline(cin, s);
 
-    auto x = prefix_fun(t);
-    auto y = prefix_fun(s);
-    auto z = prefix_fun(s + "|" + t);
+    auto origin = s + "|" + t;
+    auto n = (int) origin.length();
+    int pf[n];
+    prefix_fun(pf, origin.c_str(), n);
+    int sl = (int) s.length();
 
-    foo(z);
+    int result[200000];
+    auto result_index = 0;
 
-    cout << bullshit_index << endl;
-    for (int index = 0; index < bullshit_index; index++) {
-        cout << bullshit[index] << ' ';
+    for (int i = n - 1; i >= 2 * sl; i--) {
+        if (pf[i] == sl) {
+            result[result_index++] = i - sl - sl;
+        }
     }
 
+    cout << result_index << endl;
+    for (int i = result_index-1; i >= 0; i--) {
+        cout << result[i] << ' ';
+    }
 }
